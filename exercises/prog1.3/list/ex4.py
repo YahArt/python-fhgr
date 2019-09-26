1de6866fa5793747c3cd7857d43eb51f011f83d7
hutter <hutteryannick@gmail.com>
2019-09-26 15:45:05 +0200

    Finished list exercises

---
+++ exercises/prog1.3/list/ex4.py
@@ -0,0 +1,5 @@
+# Satz engegennehmen
+satz = input("Bitte geben Sie einen Satz ein: ")
+wort_liste = satz.split()
+wort_anzahl = len(wort_liste)
+print("Dieser Satz beinhaltet", wort_anzahl, "Wörter.")
---
+++ exercises/prog1.3/list/ex5.py
@@ -0,0 +1,7 @@
+mein_text = """Ich bin Satz 1
+Ich bin Satz 2
+Ich bin Satz 3"""
+
+satz_liste = mein_text.split('\n')
+satz_anzahl = len(satz_liste)
+print("Dieser Text beinhaltet", satz_anzahl, "Sätze.")
