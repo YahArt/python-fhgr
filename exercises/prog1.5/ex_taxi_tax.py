e26706e3bd6907d0b51e08e8b9279afb80c815a2
hutter <hutteryannick@gmail.com>
2019-10-03 14:47:15 +0200

    Completed taxi exercise

---
+++ exercises/prog1.5/ex_taxi_tax.py
@@ -0,0 +1,23 @@
+def berechne_taxi_preis(tageszeit, distanz):
+    start_preis = 0
+    preis_pro_kilometer = 0
+
+    if tageszeit == "Tag":
+        start_preis = 8
+        preis_pro_kilometer = 3
+    else:
+        # Wir nehmen an es ist Nacht
+        start_preis = 10
+        preis_pro_kilometer = 4
+
+    # Die ersten zwei Kilometer sind gratis
+    distanz = distanz - 2
+    if distanz <= 0:
+        print("Der Preis in der Tageszeit", tageszeit,  "beträgt", start_preis)
+    else:
+        preis = start_preis + distanz * preis_pro_kilometer
+        print("Der Preis in der Tageszeit", tageszeit, "mit der Distanz", distanz, "km beträgt", preis)
+
+tageszeit = input("Tageszeit ('Tag' oder 'Nacht'): ")
+distanz = int(input("Die Strecke in Kilometer: "))
+berechne_taxi_preis(tageszeit, distanz)
